4415772af572b706a40bc620cd5c56c9641154d5
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2023-12-20 18:23:27 +0530

    Update background and search width in App.css, add MovieCard component and search functionality in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import { useEffect } from "react";
+import MovieCard from "./MovieCard";
 import './App.css';
+import SearchIcon from './search.svg';
+
 
 const API_URL = 'https://www.omdbapi.com?apikey=6ad9847';   // API URL
 
+const movie1 = {
+    "Poster" : "https://m.media-amazon.com/images/M/MV5BZDlmMGQwYmItNTNmOS00OTNkLTkxNTYtNDM3ZWVlMWUyZDIzXkEyXkFqcGdeQXVyMTA5Mzk5Mw@@._V1_SX300.jpg",
+    "Title" : "Spiderman in Cannes",
+    "Type" : "movie",
+    "Year" : "2016",
+    "imdbID" : "tt5978586"
+}
 
 const App = () => {
 
@@ -17,7 +27,17 @@ const App = () => {
     }, []);
 
     return (
-        <h1>Hello World</h1>
+        <div className="app">
+            <h1>Movie Land</h1>
+            <div className="search">
+                <input placeholder="Search for movies..." value="Superman" onChange={() => {}}/>
+                <img src = {SearchIcon} alt="Search Icon" onClick={() => {}}/>
+            </div>
+
+            <div className="container">
+                <MovieCard movie1 = {movie1}/>
+            </div>
+        </div>
     );
 }
 
